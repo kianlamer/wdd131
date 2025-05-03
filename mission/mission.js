bf23895e9f5bef95f86ebfcf5e51324e8c2ed6bb
kianlamer <kianlamer123@outlook.com>
2025-05-02 21:30:03 -0600

    Added dark mode to the mission statement

--- mission/mission.js
+++ mission/mission.js
@@ -7,10 +7,12 @@ function changeTheme() {
     let current = SelectElem.value;
     if (current == "dark") {
         document.body.className = "dark";
+        logo.src = "byui-logo_white.png";
+        }
+    else if (current == "light") {
+        document.body.className = "light";
+        logo.src = "byui-logo_blue.webp";
     }
-    else {
-        
-    }
-        
-    
 }
+
+SelectElem.addEventListener("change", changeTheme);
